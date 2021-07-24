b7bdb0b0211c36fa0c981064a1d9ff37abb5cbb9
Technion <technion@lolware.net>
2021-07-24 18:39:40 +1000

    Added all mappings to install file

--- OpenSafetyInstall.ps1
+++ OpenSafetyInstall.ps1
@@ -7,7 +7,18 @@ If (-not (Test-Path $installpath)) {
 
 Copy-Item .\target\debug\open_safety.exe 'C:\Program Files\open_safety' -Force
 
-#cmd /c assoc .js
+# List from application: allowed_extensions = ["js", "jse", "vbs", "wsf", "wsh", "hta"];
+# Obtained existing names with: cmd /c assoc .ext
 # .js=JSFile
+# .jse=JSEFile
+# .vbs=VBSFile
+# .wsf=WSFFile
+# .wsh=WSHFile
+# .hta=htafile
 
 cmd /c ftype JSFile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
+cmd /c ftype JSEFile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
+cmd /c ftype VBSFile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
+cmd /c ftype WSFFile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
+cmd /c ftype WSHFile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
+cmd /c ftype htafile=`"C:\Program Files\open_safety\open_safety.exe`" `"%1`"
